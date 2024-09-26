846a3f0cdeb785b8a62da81e8d567bd8feb316f1
martaaa-16 <martaprama@gmail.com>
2024-09-26 11:19:24 +0700

    Upload file

---
+++ PemilihanHari15.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PemilihanHari15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dayName, dayType;
+    
+        System.out.println("Input day name: ");
+        dayName = sc.nextLine();
+    
+        switch (dayName.toLowerCase()) {
+            case "monday":
+            case "tuesday":
+            case "wednesday":
+            case "thursday":
+            case "friday":
+                dayType = " Weekday";
+                break;
+            case "saturday":
+            case "sunday":
+                dayType = "Weekend";
+                break;
+            default:
+                dayType = "invalid day name";
+        }
+        System.out.println(dayName + " is a " + dayType);
+        
+    }
+    
+}
